e5ad82ec2f37f293bbb8201b00b745e804ebec86
squirrel <vladimirkl2005@gmail.com>
2018-05-20 17:13:43 +0700

    instalisation

---
+++ src/test/java/test.java
@@ -0,0 +1,39 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class test {
+
+    @Test
+    public void testToWikipediaWebSite(){
+        System.setProperty("webdriver.chrome.driver","D:\\chromedriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.avsu.ru/loginparent/");
+
+        WebElement login=driver.findElement(By.cssSelector(".loginTeacher"));
+        login.clear();
+        login.sendKeys("9635203775");
+
+        WebElement password=driver.findElement(By.id("avsu_pass"));
+        password.clear();
+        password.sendKeys("30Djdrf05");
+
+        WebElement enter=driver.findElement(By.id("loginTeacherSubmit"));
+        enter.click();
+
+
+        WebElement balanse=driver.findElement(By.className("dataHeaderLcDiv4"));
+        balanse.getText();
+
+
+    }
+
+
+
+
+}
+
